ef762b2edc277b4f9d8e125739a8236a3930c4d4
Michael <michael.cullen81@gmail.com>
2021-10-09 14:38:01 +0100

    Initial commit

---
+++ ChatApp/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,120 @@
+﻿using ChatApp.Core;
+using ChatApp.MVVM.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace ChatApp.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+        public ObservableCollection<MessageModel> Messages { get; set; }
+        public ObservableCollection<ContactModel> Contacts { get; set; }
+
+        public RelayCommand SendCommand { get; set; }
+
+
+
+        public ContactModel SelectedContact { 
+            get => _selectedContact; 
+            set { 
+                _selectedContact = value; 
+                OnPropertyChanged(); 
+            } 
+        }
+
+        private string _message;
+        private ContactModel _selectedContact;
+
+        public string Message
+        {
+            get { return _message; }
+            set
+            {
+                _message = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+
+        public MainViewModel()
+        {
+            Messages = new ObservableCollection<MessageModel>();
+            Contacts = new ObservableCollection<ContactModel>();
+
+            SendCommand = new RelayCommand(o =>
+            {
+                Messages.Add(new MessageModel
+                {
+                    Message = Message,
+                    FirstMessage = false
+                });
+
+                Message = "";
+            });
+
+            Messages.Add(new MessageModel
+            {
+                Username = "Allison",
+                UsernameColor = "#409AFF",
+                ImageSource = "https://i.imgur.com/yMWvLXd.png",
+                Message = "Test",
+                Time = DateTime.Now,
+                IsNativeMessage = false,
+                FirstMessage = true
+            });
+
+            for (int i = 0; i < 3; i++)
+            {
+                Messages.Add(new MessageModel
+                {
+                    Username = "Allison",
+                    UsernameColor = "#409AFF",
+                    ImageSource = "https://i.imgur.com/yMWvLXd.png",
+                    Message = "Test",
+                    Time = DateTime.Now,
+                    IsNativeMessage = false,
+                    FirstMessage = true
+                });
+            }
+
+            for (int i = 0; i < 4; i++)
+            {
+                Messages.Add(new MessageModel
+                {
+                    Username = "Bunny",
+                    UsernameColor = "#409AFF",
+                    ImageSource = "https://i.imgur.com/yMWvLXd.png",
+                    Message = "Test",
+                    Time = DateTime.Now,
+                    IsNativeMessage = true,
+                    FirstMessage = true
+                });
+            }
+
+            Messages.Add(new MessageModel
+            {
+                Username = "Allison",
+                UsernameColor = "#409AFF",
+                ImageSource = "https://i.imgur.com/yMWvLXd.png",
+                Message = "Last",
+                Time = DateTime.Now,
+                IsNativeMessage = false,
+                FirstMessage = true
+            });
+
+            for (int i = 0; i < 5; i++)
+            {
+                Contacts.Add(new ContactModel
+                {
+                    Username = $"Allison {i}",
+                    ImageSource = "https://i.imgur.com/i2szTsp.png",
+                    Messages = Messages
+                });
+            }
+
+        }
+    }
+}
